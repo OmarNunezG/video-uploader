58b83ff02c2655ba98a8bedbbf2f35e8872beed9
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2023-03-01 11:22:05 -0400

    Added user endpoints
    /api/users/
    Allowed methods: GET, POST
    
    /api/users/:id/
    Allowed methods: GET, PATCH, DELETE
    
    /api/users/:id/videos/
    Allowed methods: GET
    
    /api/users/me/
    Allover methods: GET, PATCH

--- backend/app/user/urls.py
+++ backend/app/user/urls.py
@@ -4,10 +4,18 @@ from user import views
 app_name = "user"
 
 urlpatterns = (
-    path("token/", views.PairTokenView.as_view(), name="token_obtain_pair"),
+    path("token/", views.PairTokenView.as_view(), name="pair-token"),
     path(
         "token/refresh/",
         views.RefreshTokenView.as_view(),
-        name="token_refresh",
+        name="refresh-token",
     ),
+    path("", views.UserListView.as_view(), name="list"),
+    path("<int:user_id>/", views.UserDetailView.as_view(), name="detail"),
+    path(
+        "<int:user_id>/videos/",
+        views.UserVideosView.as_view(),
+        name="videos",
+    ),
+    path("profile/", views.UserProfileView.as_view(), name="profile"),
 )
